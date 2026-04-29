610b19b7b7ac37b3de1d207eb730094108672dcc
kinokombads <42504644+kinokombads@users.noreply.github.com>
2026-04-30 01:05:34 +0800

    Created Model Layer
    - created Product and ProductType class

---
+++ Pos.MauiView/Pos.MauiView/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Pos.MauiView;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ Pos.MauiView/Pos.Model/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace Pos.Model;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string ProductCode { get; set; }
+    public string Name { get; set; }
+    public string Details { get; set; }
+    public int SortOrder { get; set; }
+    public decimal WholePrice { get; set; }
+    public decimal RetailPrice { get; set; }
+    public int StatId { get; set; }
+
+
+    public Product()
+    {
+        ProductId = 0;
+        ProductCode = "";
+        Name = "";
+        Details = "";
+        SortOrder = 0;
+        WholePrice = 0;
+        RetailPrice = 0;
+        StatId = 0;
+    }
+
+}
