ff7175ff008266a1da0907729bdf258c2522ffbb
kinokombads <42504644+kinokombads@users.noreply.github.com>
2026-04-29 23:45:18 +0800

    initial push
    - created the project

---
+++ Pos.MauiView/Pos.MauiView/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Pos.MauiView;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "Pos.MauiView" };
+    }
+}
---
+++ Pos.MauiView/Pos.MauiView/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Pos.MauiView;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
